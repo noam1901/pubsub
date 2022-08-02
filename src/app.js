9f895f6ec1a67cf1c55262ef50f0f9dcbe503a7c
noam meir <noamariel8@gmail.com>
2022-08-02 14:44:33 +0300

    added delay

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import Mersenne from "./clients/mersenne.js";
 import primeEndsWithOne from "./clients/prime1.js";
 import fourDigits from "./clients/prime4Digits.js";
 
-const primes = new Prime(2000)
+const primes = new Prime(100)
 const mersenne = new Mersenne()
 const end1 = new primeEndsWithOne()
 const fourdigits = new fourDigits
@@ -13,8 +13,6 @@ primes.on('start', mersenne.isMersenne)
 primes.on('start', end1.isEndsWithOne)
 primes.on('start', fourdigits.is4Digits)
 
-for(let number of primes.generator()){
-
-}
+primes.generator()
 
 
--- src/publishers/PrimeGenerator.js
+++ src/publishers/PrimeGenerator.js
@@ -1,24 +1,34 @@
 import Events from "../myEvent.js";
+import chalk from 'chalk'
 class Prime extends Events{
     constructor(max){
         super()
         this.buttomNumber = 2
         this.max = max
     }
-    *generator(){
-        for(let i=this.buttomNumber; i<this.max;i++){
-            if(this.isPrime(i)){
-                yield i
-                this.emit('start', i)
-            }
+    generator(){
+        let flag = false
+        if (this.buttomNumber===this.max){
+            return
+        }
+        if(this.isPrime(this.buttomNumber)){
+            this.emit('start',this.buttomNumber)
+            console.log(chalk.white(this.buttomNumber));
+            flag = true
+        }
+        
+        this.buttomNumber++
+        if(flag){
+            setTimeout(()=> this.generator(),1000)
+        }
+        else{
+            this.generator()
         }
     }
     isPrime(num){
         for(let i = 2, s = Math.floor(Math.sqrt(num)); i <= s ; i++ )
             if(num % i === 0) return false;
         return num > 1;
-            
-        
     }
 }
 
