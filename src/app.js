1ae08cd1d3e36794b81929e80f83303e08afa386
noam meir <noamariel8@gmail.com>
2022-08-02 12:30:07 +0300

    added 4 digits

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import Prime from "./publishers/PrimeGenerator.js";
 import Mersenne from "./clients/mersenne.js";
 import primeEndsWithOne from "./clients/prime1.js";
+import fourDigits from "./clients/prime4Digits.js";
 
 const primes = new Prime(32)
 const mersenne = new Mersenne()
 const end1 = new primeEndsWithOne()
+const fourdigits = new fourDigits
 
 for(let value of primes.generator()){
-    console.log(value, mersenne.isMersenne(value), end1.isEndsWithOne(value) );
+    console.log(value, mersenne.isMersenne(value), end1.isEndsWithOne(value), fourdigits.is4Digits(value) );
 }
 
 
