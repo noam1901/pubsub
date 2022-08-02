9d114e649e4939d3861d2500ff4cebe729cd9329
noam meir <noamariel8@gmail.com>
2022-08-02 12:23:28 +0300

    added prime1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import Prime from "./publishers/PrimeGenerator.js";
 import Mersenne from "./clients/mersenne.js";
+import primeEndsWithOne from "./clients/prime1.js";
 
 const primes = new Prime(32)
 const mersenne = new Mersenne()
+const end1 = new primeEndsWithOne()
 
 for(let value of primes.generator()){
-    console.log(value, mersenne.isMersenne(value));
+    console.log(value, mersenne.isMersenne(value), end1.isEndsWithOne(value) );
 }
 
 
